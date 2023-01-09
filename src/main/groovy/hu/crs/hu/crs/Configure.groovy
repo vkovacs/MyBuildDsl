da8287fbd5ca44bbf218e607a2f716ad11d0036a
Viktor Kovacs <crusader8@gmail.com>
2023-01-09 18:00:40 +0100

    update gradle version

--- src/main/groovy/hu/crs/hu/crs/Configure.groovy
+++ src/main/groovy/hu/crs/hu/crs/Configure.groovy
@@ -4,6 +4,7 @@ import static hu.crs.hu.crs.Build.project
 
 // https://dzone.com/articles/groovy-dsl-simple-example
 // https://www.youtube.com/watch?v=i9pNYW1Pg9A
+// https://groovy-lang.org/closures.html#_delegate_of_a_closure
 project {
     name = "asd"
     version = "1.0.0"
